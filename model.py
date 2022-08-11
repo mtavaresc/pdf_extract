5dee65710b59f26d5d38b8f2b1ebd9cd8028687a
mtavaresc <marcelo.t498@gmail.com>
2022-08-11 19:13:18 -0300

    Initial

---
+++ model.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class Record:
+    data: date
+    historico: str
+    custo_receita: str
+    recebimentos: float
+    pagamentos: float
+    nota: int = None
+    serie: int = None
+    parc: int = None
+    parceiro: int = None
+    vencimento: date = None
+    valor_titulo: float = None
+    valor_recebido: float = None
+    valor_pago: float = None
+    saldo: float = None
